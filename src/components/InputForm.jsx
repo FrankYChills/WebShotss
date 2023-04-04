2837c689e70c60a10f765aa540a0d3554b738583
franky <itskchills11@gmail.com>
2023-04-04 14:31:59 +0530

    display image and download option

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
+import getImage from "../getImage";
 
 const InputForm = () => {
   const [url, setUrl] = useState("");
   const [key, setKey] = useState("");
+  const [isLoading, setLoading] = useState(false);
+  const [image, setImage] = useState("");
+  const [gotImage, setGotImage] = useState(false);
+
   const handleSubmit = async (e) => {
+    setGotImage(false);
     e.preventDefault();
-    console.log("submit clicked");
+    setLoading(true);
+    var uri = await getImage(url, key);
+    setImage(uri);
+    setLoading(false);
+    setGotImage(true);
+    setUrl("");
   };
 
   return (
-    <div>
+    <div className="main_div">
       <form className="shot_form">
         <div className="fm_inp">
           <label htmlFor="url">URL</label>{" "}
@@ -34,10 +45,33 @@ const InputForm = () => {
             placeholder="Enter Your API Key"
           />
         </div>
-        <button type="submit" onClick={handleSubmit} className="btn">
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="btn"
+          disabled={isLoading}
+        >
           Get Image
         </button>
+        {gotImage ? (
+          <a href={image} download className="down_image">
+            Download Image
+          </a>
+        ) : (
+          ""
+        )}
       </form>
+      <div className="img_div">
+        {!isLoading && !gotImage ? (
+          <h2>Your Image will be shown here</h2>
+        ) : isLoading && !gotImage ? (
+          <h2>Loading ...</h2>
+        ) : !isLoading && gotImage ? (
+          <img src={image} className="res_img" />
+        ) : (
+          <h2>No image found!!</h2>
+        )}
+      </div>
     </div>
   );
 };
--- src/getImage.js
+++ src/getImage.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 
-export default async function getImage(key) {
-  var url = "https://website-screenshot-api.exponential.host/";
+export default async function getImage(url, key) {
+  var expo_url = "https://website-screenshot-api.exponential.host/";
 
-  const res = await axios.get(url, {
+  const res = await axios.get("https://proxy.cors.sh/" + expo_url, {
     headers: {
+      "x-cors-api-key": "temp_b5979cdab1a0140dd639c78650e1fee5",
       exponential_api_secret: key,
     },
+    params: {
+      url: url,
+    },
+    responseType: "blob",
   });
-
   console.log(res);
+
+  let objectURL = URL.createObjectURL(res.data);
+  console.log(objectURL);
+  return objectURL;
 }
